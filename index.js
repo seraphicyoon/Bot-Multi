1120c86a05bf9c705fc89de8b0d6ea87b73cf0f2
seraphicyoon <yoonmendo@gmail.com>
2026-02-12 11:30:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -21,14 +21,20 @@ const client = new Client({
 
 const KISS_GIFS = {
   hetero: [
-    "https://media.tenor.com/2roX3uxz_68AAAAC/anime-kiss.gif"
+    "https://media.tenor.com/2roX3uxz_68AAAAC/anime-kiss.gif",
   ],
   lesbica: [
-    "https://media.tenor.com/Z6gmDPeM6dgAAAAC/anime-lesbian-kiss.gif"
+    "https://media.tenor.com/Z6gmDPeM6dgAAAAC/anime-lesbian-kiss.gif",
   ],
   gay: [
-    "https://media.tenor.com/3wvXbYpY0QMAAAAC/anime-boy-kiss.gif"
+    "https://media.tenor.com/3wvXbYpY0QMAAAAC/anime-boy-kiss.gif",
   ],
 };
 
-const TYPES = ["heter]()
+const TYPES = ["hetero", "lesbica", "gay"];
+
+function random(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+/* ========*
