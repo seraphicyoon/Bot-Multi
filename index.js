5a706b9d8260d7096ff6541f9ec9e8395ea86249
seraphicyoon <yoonmendo@gmail.com>
2026-02-12 11:26:07 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const {
+  Client,
+  GatewayIntentBits,
+  REST,
+  Routes,
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+/* ===============================
+   GIFS
+================================ */
+
+const KISS_GIFS = {
+  hetero: [
+    "https://media.tenor.com/2roX3uxz_68AAAAC/anime-kiss.gif"
+  ],
+  lesbica: [
+    "https://media.tenor.com/Z6gmDPeM6dgAAAAC/anime-lesbian-kiss.gif"
+  ],
+  gay: [
+    "https://media.tenor.com/3wvXbYpY0QMAAAAC/anime-boy-kiss.gif"
+  ],
+};
+
+const TYPES = ["heter]()
